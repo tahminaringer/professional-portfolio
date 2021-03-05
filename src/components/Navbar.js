5563eae9987d42acf28c48ed2f3d9247fe3f8431
Tahmina <tahmina.ringer@gmail.com>
2021-03-05 14:33:12 -0800

    added basic nav functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,10 +2,40 @@ import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
   return (
     <nav className="navbar">
       <div className="navbar-container">
         <Link to="/" className="navbar-logo"></Link>
+        Tahmina Ringer <i className='fab fa-typo3' />
+        <div className="menu-icon" onClick={handleClick}>
+          <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+        </div>
+        <ul className={click ? "nav-menu active" : 'nav-menu'}>
+          <li className="nav-item">
+            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="resume" className="nav-links" onClick={closeMobileMenu}>
+              Resume
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="dev-portfolio" className="nav-links" onClick={closeMobileMenu}>
+              Dev Portfolio
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="floral-portfolio" className="nav-links" onClick={closeMobileMenu}>
+              Floral Portfolio
+            </Link>
+          </li>
+        </ul>
       </div>
     </nav>
   )
