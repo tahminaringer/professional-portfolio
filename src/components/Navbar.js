3c1c84c63cf48c34786bc1deabaf6e3bdd7bd6ae
Tahmina <tahmina.ringer@gmail.com>
2021-03-08 12:01:28 -0800

    added a signup button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,25 @@
 import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
+import { Button } from './Button';
+import './Navbar.css';
 
 function Navbar() {
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if(window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  window.addEventListener("resize", showButton);
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
@@ -34,8 +48,12 @@ function Navbar() {
             <Link to="floral-portfolio" className="nav-links" onClick={closeMobileMenu}>
               Floral Portfolio
             </Link>
+          <li className="nav-item">
+            <Link to="/sign-up" className="nav-links-mobile" onClick={closeMobileMenu}>Sign Up</Link>
+          </li>
           </li>
         </ul>
+        {button && <Button buttonStyle="btn--outline">SIGN UP</Button>}
       </div>
     </nav>
   )
